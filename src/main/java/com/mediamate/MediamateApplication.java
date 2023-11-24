4d08abb6808a372db42ab706c08a285af8803966
Filip Szała <filip.szala96@gmail.com>
2023-11-24 13:41:19 +0100

    structure of project added

---
+++ src/main/java/com/mediamate/MediamateApplication.java
@@ -0,0 +1,13 @@
+package com.mediamate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediamateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediamateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediamate/MediamateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediamate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediamateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
